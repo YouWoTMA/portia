e2f19e9dc707bf9d321e6dd70b1b0372b3218507
David Bengoa <david@bengoarocandio.com>
2015-07-27 14:37:32 +0700

    Fix slyd backend tests (WIP)

--- slyd/slyd/bot.py
+++ slyd/slyd/bot.py
@@ -184,6 +184,7 @@ class Fetch(BotResource):
         except IOError as ex:
             if ex.errno == errno.ENOENT:
                 log.msg("skipping extraction, no spec: %s" % ex.filename)
+                return None, None
             else:
                 raise
 
